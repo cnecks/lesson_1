f1e1f076b9881a3ec963456403e50c44b7bf58ac
cnecks <98271194+cnecks@users.noreply.github.com>
2022-01-30 18:57:08 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,70 @@
-usd = float(2.57)
-euro = float(2.92)
+# print("Hello")
+#
+# fname = 'Matvei'
+# lname = 'Skrigalovski'
+#
+# print(fname , lname)
+#
+# input('Нажмите Enter')
 
-money = int(input('Введите сумму,которую хотите обменять: '))
-currency = int(input('1 - доллары, 2 - евро: '))
+# a = input('Введите свое имя: ')
+# b = int(input('ведите ваш возраст: '))
+#
+# print(type(a)
+# print(type(b)
 
-if currency == 1:
-    cash = round(money / usd, 2)
-    print(cash)
-elif currency == 2:
-    cash = round(money / euro, 2)
-    print(cash)
+# print("\ta")
+
+# print('''"Тут что-то не так, не будь я д'Артаньян"-подумал он."''')
+
+# in
+# True=0 False=1
+# s = 10, 20, 30
+# x = 5
+# print(x in s)
+
+# list (список) -> [], изменяемый
+# tuple (кортеж) ->, неизменяемый"""
+
+# string = "Всем привет, я люблю python!"
+#
+# print(f'Исходная фраза: {string}')
+
+# print(f'\nКогда я кричу: {string.upper()}')
+#
+# print(f'\nКогда я кричу: {string.lower()}')
+#
+# print(f'\nС заглавных букв: {string.title()}')
+#
+# print(f'\nС заменой: {string.replace("ЛЮБЛЮ", "ОБОЖАЮ")}')
+#
+# input('Введите Enter для завершения')
+
+# string = 'matvei', 'skrigalovski', 20
+# # print(string[0])
+# # print(string[1])
+# # print(string[2])
+#
+#
+# print(string[-1])
+# print(string[-2])
+# print(string[-3])
+
+
+string = 'matvei'
+print(string[0:3+1])
+print(string[-1])
+
+string = 'Сидел барсук в своей норе и ел картошочку пюре'
+a = '!'
+c = 'ре'
+
+print(len(string))
+print(string + a)
+
+if c in string:
+    print('ре есть!')
 else:
-    print('что-то пошло не так')
+    print(f'{c} нет!')
+
+
